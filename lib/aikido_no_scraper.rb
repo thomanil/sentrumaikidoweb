df4903633e8b92427bef1ffb8d39413159397dea
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2013-10-23 20:55:06 +0200

    ripped out old regex scrape

--- lib/aikido_no_scraper.rb
+++ lib/aikido_no_scraper.rb
@@ -16,16 +16,11 @@ module SentrumAikido
       end
     end
 
-    HEADER = "<em>Dette er et automatisk uttrekk fra NAF-kalenderen, se <a href=\"#{CALENDAR_URL}\">aikido.no</a> for mer info</em>"
-
     # Transform given raw NAF calendar html source into simplified html table we can embed on our site
-    def process_calendar(calendar_src)
-      month_chunks = get_month_chunks(calendar_src)
-      all_activities = []
-      month_chunks.each { |chunk| all_activities += process_month(chunk) }
+    def process_calendar(all_activities)
       result = <<PAGESRC
   <p>
-  #{HEADER}
+  <em>Dette er et automatisk uttrekk fra NAF-kalenderen, se <a href=\"#{CALENDAR_URL}\">aikido.no</a> for mer info</em>
   </p>
 
   <table border="0" id="scraped-calendar">
@@ -35,22 +30,6 @@ module SentrumAikido
 PAGESRC
     end
 
-    # Takes full html src of the calendar.
-    # Extract each month from calendar src, returns array of strings each of which is a month "chunk" of markup
-    def get_month_chunks(calendar_src)
-      months = []
-      pattern = /<td class="month">(.*?)(?=<td class="month">)/m
-      calendar_src =~ pattern
-
-      while($1 != nil) do
-        month = $1
-        # Put month td elements back, they are consumed by pattern match otherwise
-        months.push("<td class=\"month\">"+month+"<td class=\"month\">")
-        $' =~ pattern
-      end
-
-      return months
-    end
 
     # Takes array of activity hashmaps
     # Returns one html table row per activity, one column per key-value pair of activity (when, where, etc)
@@ -106,56 +85,8 @@ PAGESRC
       return adr
     end
 
-    # Takes month string description and an activity chunk of markup,
-    # Each activity element (when, where, who etc) is a table row in this format:
-    #   <tr><td class="activitylegend">Hva:</td><td class="activity"><b>Seminar med Birger Sørensen 5.dan</b></td></tr>
-    # Extract each of its rows from markup chunk and store in hash keys: [what, when, place, contact, moreinfo]
-    # Order of activity fields is significant, we rely on them being consistently ordered in the NAF calendar
-    # Returns hashmap of the activites elements
-    def process_activity(activity_chunk, month)
-      fields = []
 
-      pattern = /<tr><td class="activitylegend">.*?<\/td><td class="activity">(.*?)<\/td><\/tr>/m
-      activity_chunk =~ pattern
 
-      while($1 != nil) do
-        field = $1.strip
-        fields.push(field)
-        $' =~ pattern
-      end
-
-      activity = {:time => (fields[1]+" "+month.downcase()), :place => fields[2], :activity => fields[0],
-        :contact => fields[4], :moreinfo => fields[5]}
-
-      return activity
-    end
-
-    # Takes a chunk of markup starting with <td class="month"> element, extracts month name and cuts it up into activity chunks.
-    # Each activity "chunk"  starts and ends with markup: "<tr><td class="activity">"
-    # returns array of activity hashmaps
-    def process_month(month_chunk)
-      pattern =  /<td class="month"><b>(.*?)<\/b><\/td>/
-      month_chunk =~ pattern
-      month = $1.downcase
-      month = month.gsub(/<b>/, "")
-      month = month.gsub(/<\/b>/, "")
-
-      activityChunks = []
-
-      pattern =  /<tr><td class="activity">(.*?)<tr><td class="activity">/m
-      month_chunk =~ pattern
-      while($1 != nil) do
-        activity = $1
-        activityChunks.push(activity)
-        $' =~ pattern
-      end
-
-      collected = []
-      counter = 0
-      activityChunks.each { |chunk| collected.push(process_activity(chunk, month))}
-
-      return collected
-    end
 
     ERRORPREFIX = "<em>Det oppstod en uventet feil under uttrekk av NAF-kalender. Si gjerne ifra til Thomas! :)!</em>"
 
@@ -171,7 +102,12 @@ END_OF_MESSAGE
     # If any exceptions are raised during execution a human readable error message is returned instead.
     def scrape_calendar
       begin
-        return process_calendar(get_calendar_src)
+
+        src = get_calendar_src
+        activities = [{:time => "12.januar", :place => "Oslo", :activity => "Leir med Hodor",
+        :contact => "Mr T", :moreinfo => "call us"}]
+
+        return process_calendar(activities)
       rescue Exception => e
         return format_error(e)
       end
