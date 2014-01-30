899237cc170428840edcbe8a60de2dfdb0403618
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2014-01-30 23:14:08 +0100

    Prep for adding separate mobile content, and prep mobile nav content

--- sentrumaikido.rb
+++ sentrumaikido.rb
@@ -9,101 +9,88 @@ require './lib/cms.rb'
 def nav_items
   [
     {:section => "Ny her?",
-      :beginners => ["/beginners", "Nybegynnerkurs"],
-      :intro => ["/", "Introduksjon"],
-      :about_aikido => ["/about_aikido", "Om Aikido"],
-      :about_dojo => ["/about_dojo", "Om klubben"]
-    },
-
-    {:section => "Ny her?",
-      :beginners => ["/beginners", "Nybegynnerkurs"],
-      :intro => ["/", "Introduksjon"],
-      :about_aikido => ["/home/about_aikido", "Om Aikido"],
-      :about_dojo => ["/home/about_dojo", "Om klubben"],
+      :links => {
+        :beginners => ["/beginners", "Nybegynnerkurs"],
+        :intro => ["/", "Introduksjon"],
+        :about_aikido => ["/home/about_aikido", "Om Aikido"],
+        :about_dojo => ["/home/about_dojo", "Om klubben"],
+      }
     },
 
     {:section => "Aktiviteter",
-      :classes => ["/home/classes", "Treningstider"],
-      :calendar => ["/calendar", "Kalender"],
+      :links => {
+        :classes => ["/home/classes", "Treningstider"],
+        :calendar => ["/calendar", "Kalender"]
+      }
     },
 
     {:section => "Praktisk",
-      :rates => ["/home/rates", "Priser"],
-      :contact => ["/home/contact", "Kontaktinfo"],
-      :instructors => ["/home/instructors", "Instruktører"],
-      :board => ["/home/board", "Styret"],
-      :social => ["/home/social", "Sosialt"],
+      :links => {
+        :rates => ["/home/rates", "Priser"],
+        :contact => ["/home/contact", "Kontaktinfo"],
+        :instructors => ["/home/instructors", "Instruktører"],
+        :board => ["/home/board", "Styret"],
+        :social => ["/home/social", "Sosialt"]
+      }
     },
 
     {:section => "Lær mer",
-      :faq => ["/home/faq", "FAQ"],
-      :advice => ["/home/advice", "Gode råd"],
-      :personligheter => ["/home/personalities", "Personer"],
-      :etiquette => ["/home/etiquette", "Etikette"],
-      :curriculum => ["/home/curriculum", "Pensum"],
-      :terms => ["/home/terms", "Ordliste"],
-      :links => ["/home/links", "Lenker"],
-      :english => ["/home/english", "In English"],
-      :rss => ["/rss/everything?format=rss", "RSS"],
+      :links => {
+        :faq => ["/home/faq", "FAQ"],
+        :advice => ["/home/advice", "Gode råd"],
+        :personligheter => ["/home/personalities", "Personer"],
+        :etiquette => ["/home/etiquette", "Etikette"],
+        :curriculum => ["/home/curriculum", "Pensum"],
+        :terms => ["/home/terms", "Ordliste"],
+        :links => ["/home/links", "Lenker"],
+        :english => ["/home/english", "In English"],
+        :rss => ["/rss/everything?format=rss", "RSS"]
+      }
     }
   ]
 end
 
 def nav_mobile_content
-  "WATWAT"
+  mobile_links = [:beginners,
+    :about_aikido,
+    :about_dojo,
+    :classes,
+    :rates,
+    :contact,
+    :english]
+
+  all_links = []
+  nav_items.each do |section|
+    section[:links].each_pair do |key,value|
+      all_links << [key, value]
+    end
+  end
+
+  # only include the mobile_links
+  all_links = all_links.select {|tuple| mobile_links.include?(tuple[0])}
+
+  nav_content = ""
+  nav_content += "<ul class='menypunkter'>"
+  all_links.each do |link|
+    nav_content += "  <li><a href='#{link[1][0]}'>#{link[1][1]}</a></li>"
+  end
+  nav_content += "</ul>"
 end
 
 def nav_desktop_content
-  <<NAV
+  nav_content = ""
 
-	  <h2 class="menyheader">Ny her?</h2>
-	  <ul class="menypunkter">
-	    <li><a href="/beginners">Nybegynnerkurs</a></li>
-	    <li><a href="/">Introduksjon</a></li>
- 	    <li><a href="/home/about_aikido">Om Aikido</a></li>
-	    <li><a href="/home/about_dojo">Om klubben</a></li>
-	  </ul>
+  nav_items.each do |section|
+    nav_content += "<h2 class='menyheader'>#{section[:section]}</h2>"
+    nav_content += "  <ul class='menypunkter'>"
+    section[:links].each do |key, value|
+      nav_content += "<li><a href='#{value[0]}'>#{value[1]}</a></li>"
+    end
+    nav_content += "  </ul>"
+  end
 
-	  <h2 class="menyheader">Aktiviteter</h2>
-	  <ul class="menypunkter">
-	    <li><a href="/home/classes">Treningstider</a></li>
-	    <li><a href="/calendar">Kalender</a></li>
-	  </ul>
-
-	  <h2 class="menyheader">Praktisk</h2>
-	  <ul class="menypunkter">
-	    <li><a href="/home/rates">Priser</a></li>
-	    <li><a href="/home/contact">Kontaktinfo</a></li>
-	    <li><a href="/home/instructors">Instruktører</a></li>
-	    <li><a href="/home/board">Styret</a></li>
-	    <li><a href="/home/social">Sosialt</a></li>
-	  </ul>
-
-	  <h2 class="menyheader">Lær mer</h2>
-	  <ul class="menypunkter">
-	    <li><a href="/home/faq">FAQ</a></li>
-	    <li><a href="/home/advice">Gode råd</a></li>
-	    <li><a href="/home/personalities">Personer</a></li>
-	    <li><a href="/home/etiquette">Etikette</a></li>
-	    <li><a href="/home/curriculum">Pensum</a></li>
-	    <li><a href="/home/terms">Ordliste</a></li>
-	    <li><a href="/home/links">Lenker</a></li>
-	  </ul>
-
-	  <br/>
-
-	  <ul>
-	    <li> <a href="/home/english">In English</a> </li>
-	    <li> <a href="/rss/everything?format=rss">RSS</a> </li>
-	  </ul>
-
-NAV
+  return nav_content
 end
-
-
-
-
-
 
 
 # If we don't do this OSI Aikido can't embed our pages in iframe
